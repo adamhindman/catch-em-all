17073f438b81fe49c3af94973ea665cd327ed2c0
Adam Hindman <adam@generalui.com>
2019-07-15 13:22:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pokemon 
+
+This project is just to explore how Svelte might make some common behavior easier. In this case, pulling down and displaying a bunch of stuff from a public API. I'm using data from [PokeAPI](https://pokeapi.co).
+
+To run:
+
+With `node` already installed, 
+
+```
+npm i
+```
+
+then
+
+```
+npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  const max = 800;
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=${max}`;
+  const getPokemons = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+    // https://flaviocopes.com/javascript-async-await-array-map/*/
+    let getSprites = async () =>
+      await Promise.all(
+        data.results.map(async pokemon => {
+          let response = await fetch(pokemon.url);
+          let data = await response.json();
+          console.log(data);
+          return {
+            name: pokemon.name,
+            types: data.types,
+            spriteUrl: data.sprites.front_default
+          };
+        })
+      );
+    return await getSprites();
+  };
+
+  let pokemons = getPokemons();
+</script>
+
+<style src="./style.styl">
+
+</style>
+
+<ul>
+  {#await pokemons}
+    <p>...waiting</p>
+  {:then pokemons}
+    {#each pokemons as pokemon}
+      <li>
+        <img src={pokemon.spriteUrl} alt={pokemon.name} />
+        <h4>{pokemon.name}</h4>
+        <ul>
+          {#each pokemon.types as type}
+            <li>{type.type.name}</li>
+          {/each}
+          <ul />
+        </ul>
+      </li>
+    {/each}
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+</ul>
